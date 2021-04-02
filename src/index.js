977b118180ddff5ccfcc73f5a9fa9650831f39de
rpaezbas <rpaezbas@rpaezbas.com>
2021-04-02 18:11:19 +0200

    working version without snapshot

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ const h = monitorEventLoopDelay({ resolution: 20 });
 const render = require('./render');
 const EventLoopChart = () => { };
 
-const terminateSignals = ['exit', 'SIGINT', 'SIGUSR1', 'SIGUSR2', 'uncaughtException', 'SIGTERM'];
-
 var data = [];
 data['p10'] = [];
 data['p25'] = [];
@@ -14,33 +12,23 @@ data['p99'] = [];
 
 h.enable();
 
-EventLoopChart.init = (frequency,path, onexit) => {
-    initTimer(frenquency);
-    if(onexit){
-        enableFinalRender(path);
-    }
+EventLoopChart.init = (interval,path) => {
+    initTimer(interval,path);
 };
 
-EventLoopChart.snapshot = () => {
+EventLoopChart.snapshot = (path) => {
     render.render(data,path);
 };
 
-const initTimer = (frenquency) => {
-    setTimeout(() => {
+const initTimer = (interval,path) => {
+    setInterval(() => {
         data['p10'].push(h.percentile(10));
         data['p25'].push(h.percentile(25));
         data['p50'].push(h.percentile(50));
         data['p95'].push(h.percentile(95));
         data['p99'].push(h.percentile(99));
-    },frequency);
-};
-
-const enableFinalRender = (path) => {
-    terminateSignals.forEach((eventType) => {
-        process.on(eventType,() => {
-            render.render(data,path);
-        });
-    });
+        render.render(data,interval,path);
+    },interval);
 };
 
 module.exports = EventLoopChart;
--- src/render.js
+++ src/render.js
@@ -5,21 +5,34 @@ const width = 800;
 const height = 600;
 
 const labels = ['p10', 'p25', 'p50', 'p95', 'p99'];
+const numOfTicks = 8;
 
 const colors = [];
-colors['p10'] = 'rgba(0,0,255,1)';
-colors['p25'] = 'rgba(255,0,255,1)';
-colors['p50'] = 'rgba(255,0,0,1)';
-colors['p95'] = 'rgba(0,255,0,1)';
-colors['p99'] = 'rgba(255,255,0,1)';
+colors['p10'] = 'rgba(92,194,204,1)';
+colors['p25'] = 'rgba(23,141,153,1)';
+colors['p50'] = 'rgba(139,255,201,1)';
+colors['p95'] = 'rgba(255,204,207,1)';
+colors['p99'] = 'rgba(204, 92, 154, 1)';
 
 
-exports.render = async (data,path) => {
+exports.render = async (data,interval,path) => {
+    console.log("rendering...");
     const configuration = {
         type: 'line',
         data: {
-            labels: labels,
+            labels: generateLabels(data,interval),
             datasets: generateDatasets(data),
+            /*
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            maxTicksLimit : 8
+                        }
+                    }]
+                }
+            }
+            */
         }
     };
     const chartJSNodeCanvas = new ChartJSNodeCanvas({ width, height, chartCallback });
@@ -42,4 +55,14 @@ const generateDatasets = exports.generateDatasets = (data) => {
 const chartCallback = (ChartJS) => {
     ChartJS.defaults.global.elements.rectangle.borderWidth = 2;
     ChartJS.defaults.scale.gridLines.color = 'rgba(0,0,0,0.1)';
+};
+
+const generateLabels = (data,interval) => {
+    var samples = data["p10"].length; // Any p would do
+    var time = interval * samples;
+    var labels = [];
+    for(var i = 0; i < numOfTicks && i < samples; i++){
+        labels.push((time / numOfTicks) * i);
+    }
+    return labels;
 };
